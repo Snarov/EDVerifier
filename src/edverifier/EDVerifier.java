431ec9968f93d462bc4b5049aa229e6e48614cd9
Vanya Kiskin <snarovivan@gmail.com>
2015-03-22 17:56:14 +0300

    First commit

---
+++ src/edverifier/EDVerifier.java
@@ -0,0 +1,23 @@
+/*
+* EDVerifier
+* BSUIR, Department of Electronics. 2015
+* Developed by Kiskin
+*
+*/
+package edverifier;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Kiskin
+ * @version 1.0
+ */
+public class EDVerifier extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+	}
+	
+}
