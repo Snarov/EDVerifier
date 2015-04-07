2473b63765d0ead9a20a740d2df3e9787430febe
Vanya Kiskin <snarovivan@gmail.com>
2015-04-08 01:55:56 +0300

    UI is building...

---
+++ src/edverifier/controller/WorkspaceController.java
@@ -0,0 +1,17 @@
+/*
+ * BSUIR, Department of Electronics. 2015
+ * Developed by Kiskin
+ *
+ */
+package edverifier.controller;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kiskin
+ */
+public class WorkspaceController{
+
+	
+	
+}
